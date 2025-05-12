cd4612293827a4d3bc0fe1e277ccb28858da33b6
Yusuf Eren Koken <yusuferenkkn@gmail.com>
2025-05-12 04:39:53 +0300

    rtsp_gstreamer dosyası başarıyla çalıştırıldı

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from modules.camera_input_siyi import get_camera_stream
+from modules.camera_input import get_camera_stream
 from modules.vision_engine import process_frame
 import cv2
 
 def main():
     rtsp_url = "rtsp://192.168.144.25:8554/main.264"
-    udp_ip = "192.168.144.100"  # Alıcı cihazın IP adresi
-    udp_port = 5005  # Alıcı cihazın portu
-    for frame in get_camera_stream(rtsp_url, udp_ip, udp_port):
+    for frame in get_camera_stream(rtsp_url):
         result = process_frame(frame, area_threshold=500, debug=True)
 
         for detection in result["detections"]:
